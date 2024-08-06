5c276007aa6d9b74814280197b55ea811968a69b
qapquiz <arm.armariya@gmail.com>
2024-08-06 19:38:06 +0700

    feat: initialize BASEDD website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import baseddBackground from '$lib/assets/BASEDD_BACKGROUND.png';
+</script>
+
+<div 
+	class="h-screen w-screen bg-repeat"
+	style="background-image: url('{baseddBackground}')"
+>
+</div>
